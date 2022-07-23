9104714f55b9fc0534c60a7aca13764196ad0c96
Alinazar Boynazarov <88520540+AlinazarBoynazarov@users.noreply.github.com>
2022-07-23 11:06:54 -0400

    Add files via upload

---
+++ brand_generatory.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator!")
+city = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print("Your band name could be: " + city + " " + pet_name)
